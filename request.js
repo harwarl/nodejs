2d3d387b8f01691ab9fc54436c7102de5b1f96fb
harwarl <harwarl87@gmail.com>
2023-02-28 09:36:59 -0800

    using https to get

---
+++ request.js
@@ -0,0 +1,16 @@
+const https = require('https');
+
+const req = https.request('https://www.google.com', (res)=>{
+res
+.on('data', (chunk)=>{
+    console.log(`data chunk ${chunk}`);
+})
+.on('error', (error)=>{
+    console.log(`error ${error}`);
+})
+.on('end', ()=>{
+    console.log('Done');
+})
+});
+
+req.end();
