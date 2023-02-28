9c2ca1bef0cb5a9e401620356f7ce3b2f645ae90
harwarl <harwarl87@gmail.com>
2023-02-28 10:42:07 -0800

    habitable planets project

--- keplarProject.js
+++ keplarProject.js
@@ -1,16 +1,31 @@
 const fs = require('fs');
-const parse = require('csv-parse');
+const { parse } = require('csv-parse');
 
 const result = [];
 
+const habitablePlanet = [];
+
+function ishabitablePlanets(planet){
+    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
+}
+
 fs.createReadStream('kepler_data.csv')
-.on('data', (data)=>{
-    result.push(data);
-})
-.on('error', (error)=>{
-    console.log(error);
-})
-.on('end', ()=>{
-    console.log(result);
-    console.log("done");
-})
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if(ishabitablePlanets(data)){
+            habitablePlanet.push(data);
+        }
+    })
+    .on('error', (error) => {
+        console.log(error);
+    })
+    .on('end', () => {
+        console.log(habitablePlanet.map((planets)=>{
+            return planets['kepler_name'];
+        }))
+        console.log(habitablePlanet);
+        console.log("done");
+    })
