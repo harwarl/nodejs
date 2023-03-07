1cb2db7d327e9598a2cf178aba13b6954b2e9cb1
harwarl <harwarl87@gmail.com>
2023-03-07 02:41:09 -0800

    setting up cookies using passport and cookie-session

--- SECURING_NODE_APPS/server.js
+++ SECURING_NODE_APPS/server.js
@@ -4,13 +4,16 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 const { Strategy } = require('passport-google-oauth20');
 require('dotenv').config();
 
 
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2
 }
 
 const AUTH_OPTIONS = {
@@ -25,10 +28,32 @@ passport.use(new Strategy(AUTH_OPTIONS, (accessToken, refreshToken, profile, don
     done(null, profile);
 }));
 
+//save session to cookie
+passport.serializeUser((user, done)=>{
+    //could as well pass in the whole user but that would make the cookie large.
+    
+    done(null, user.id); 
+})
+
+passport.deserializeUser((id, done)=>{
+    /*
+    could use a middle ware here thou
+    User.findById(id) then save user to req.user
+    */
+    done(null, id);
+})
 const app = express();
 
 app.use(helmet());
+
+app.use(cookieSession({
+    name: 'Node-Session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ]
+}))
+
 app.use(passport.initialize());
+app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
     const isLoggedIn = true;
@@ -49,7 +74,7 @@ app.get('/auth/google/callback',
     passport.authenticate('google', {
         failureRedirect: '/failure',
         successRedirect: '/',
-        session: false
+        session: true // or leave it empty
     }), (req, res, next) => {
         console.log('Oauth done');
     })
